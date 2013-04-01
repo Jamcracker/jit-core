bf93f0016e288edbf83270351c2ee6ae748748ea
prasadpnair <prasadpnair@gmail.com>
2013-04-01 16:52:48 +0530

    Added the apache license info

--- jit-core/src/test/java/com/jamcracker/jif/adapter/template/AuthenticationImpl.java
+++ jit-core/src/test/java/com/jamcracker/jif/adapter/template/AuthenticationImpl.java
@@ -1,3 +1,26 @@
+ /************************************************************************ 
+ *   Copyright [2013] [Jamcracker Inc]
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *   
+ *   
+ * @ClassName com.jamcracker.wsutil.HttpClient
+ * @version 1.0
+ * @author Prasad P Nair
+ * @date 13-Dec-2012
+ * @see
+ *   
+ /*************************************************************************/
 package com.jamcracker.jif.adapter.template;
 
 import com.jamcracker.jif.adapter.Authentication;
